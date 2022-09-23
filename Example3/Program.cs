3ac1d39ac22dfefe0e97a087356611b278325685
Ivan Maltsev <imaltsev2018i@gmail.com>
2022-09-23 13:55:09 +0300

    9Lesson3Example

--- Example3/Program.cs
+++ Example3/Program.cs
@@ -1,17 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-//Аккерман
-    static int A(int n, int m)
-    {
-        if (n < 0 || m < 0) throw new ArgumentOutOfRangeException();
-        if (n == 0) return m + 1;
-        if (m == 0) return A(n - 1, m);
-        return A(n - 1, A(n, m - 1));
-    }
-    /*
-    static void Main(string[] args)
-    {
-        
-        Console.WriteLine(A(1, 2));
-    }*/
-Console.WriteLine(A(2, 3));
+int Depth = 0;
+        int AckermannFunc(int m, int n) 
+        {
+            Depth++;
+            if (m == 0)
+                return n + 1;
+            if (n == 0)
+                return AckermannFunc(m - 1, 1);
+            return AckermannFunc(m - 1, AckermannFunc(m, n - 1));
+        }
+ Console.WriteLine(AckermannFunc(3, 2));

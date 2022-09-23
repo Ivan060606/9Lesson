193170d5f03fe72a514748b8f055b9cd8df376d7
Ivan Maltsev <imaltsev2018i@gmail.com>
2022-09-23 14:02:13 +0300

    9Lesson3Example(2)

--- Example3/Program.cs
+++ Example3/Program.cs
@@ -1,13 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int Depth = 0;
-        int AckermannFunc(int m, int n) 
-        {
-            Depth++;
-            if (m == 0)
-                return n + 1;
-            if (n == 0)
-                return AckermannFunc(m - 1, 1);
-            return AckermannFunc(m - 1, AckermannFunc(m, n - 1));
-        }
- Console.WriteLine(AckermannFunc(3, 2));
+﻿int Depth = 0;
+    int AckermannFunc(int m, int n) 
+    {
+    Depth++;
+    if (m == 0)
+        return n + 1;
+    if (n == 0)
+    return AckermannFunc(m - 1, 1);
+    return AckermannFunc(m - 1, AckermannFunc(m, n - 1));
+    }
+Console.WriteLine("Введите число М");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(AckermannFunc(M, N));
